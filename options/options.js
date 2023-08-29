cf4825312f31ff4f11afe1b1a93ebab4bb55c916
Crowdbotics <engineering@crowdbotics.com>
2023-08-29 19:33:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "skyt7485-dev-112600",
+  url: "https://skyt7485-dev-112600.botics.co",
+  api: "https://skyt7485-dev-112600.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
